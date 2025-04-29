a9aa8f26aab4b865e75b165ac9e6d94d90f56ad9
Paul Gallagher <gallagher.paul@gmail.com>
2025-04-29 13:13:05 +0800

    Step 5: multi-step dialog box

--- rust/catsay-tui/catsay/src/main.rs
+++ rust/catsay-tui/catsay/src/main.rs
@@ -1,26 +1,66 @@
 extern crate cursive;
 
-use cursive::views::{Dialog, TextView};
+use cursive::traits::Nameable;
+use cursive::views::{Checkbox, Dialog, EditView, ListView, TextView};
 use cursive::event::Key;
+use cursive::Cursive;
+
+struct CatsayOptions<'a> {
+    message: &'a str,
+    dead: bool,
+}
+
+fn input_step(siv: &mut Cursive) {
+    siv.add_layer(
+        Dialog::new()
+            .title("Please fill out the form for the cat")
+            .content(
+                ListView::new()
+                    .child("Message:", EditView::new().with_name("message"))
+                    .child("Dead?", Checkbox::new().with_name("dead")),
+            )
+            .button("OK", |s| {
+                let message = s
+                    .call_on_name("message", |t: &mut EditView| t.get_content())
+                    .unwrap();
+                let is_dead = s
+                    .call_on_name("dead", |t: &mut Checkbox| t.is_checked())
+                    .unwrap();
+                let options = CatsayOptions {
+                    message: &message,
+                    dead: is_dead,
+                };
+                result_step(s, &options)
+            }),
+    );
+}
+
+fn result_step(siv: &mut Cursive, options: &CatsayOptions) {
+    let eye = if options.dead { "x" } else { "o" };
+
+    let cat_text = format!(
+        "{message}
+ \\
+  \\
+    /\\_/\\
+   ( {eye}.{eye} )
+    > ^ <",
+        message = options.message,
+        eye = eye
+    );
+
+    siv.pop_layer();
+    siv.add_layer(
+        Dialog::around(TextView::new(cat_text))
+            .title("The cat says...")
+            .button("OK", |s| s.quit()),
+    );
+}
 
 fn main() {
     let mut siv = cursive::crossterm();
-    let cat_text = "
-{message}
-\\
- \\
-   /\\_/\\
-  ( {eye}.{eye} )
-   > ^ <";
 
-    let cat_template = cat_text.replace("{message}", "Meow!");
-    let cat_template = cat_template.replace("{eye}", "o");
-
-    siv.add_layer(
-        Dialog::around(TextView::new(cat_template))
-            .title("The cat says...")
-            .button("OK", |s| s.quit())
-    );
+    input_step(&mut siv);
 
     siv.add_global_callback(Key::Esc, |s| s.quit()); // listen for ESC key and quit
 
