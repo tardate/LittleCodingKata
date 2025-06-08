56fb7420bb9c300758f108cd98c73d4f83ac9f8c
Paul Gallagher <gallagher.paul@gmail.com>
2025-06-08 23:05:12 +0800

    [bash/loops] extend while examples

--- bash/loops/while.sh
+++ bash/loops/while.sh
@@ -4,10 +4,35 @@
 #
 n1=0
 n2=${1:-5}
-while [ $n1 -le $n2 ]
-do
-  echo "loop with n1==${n1}"
-  n1=$(($n1+1))
-done
+style=${2:-"default"}
+
+case $style in
+  "c-style-increment")
+    echo "loop while -le ${n2} with c-style increment"
+    while [ $n1 -le $n2 ]
+    do
+      echo "n1==${n1}"
+      ((n1++))
+    done
+    ;;
+  "c-style-inline-decrement")
+    n1=${n2}
+    n2=0
+    echo "loop from ${n1} to ${n2} with c-style inline decrement"
+    while ((n1--))
+    do
+      echo "n1==${n1}"
+    done
+    ;;
+  *)
+    echo "loop while -le ${n2}"
+    while [ $n1 -le $n2 ]
+    do
+      echo "n1==${n1}"
+      n1=$(($n1+1))
+    done
+    ;;
+esac
+
 
 echo "finished now n1==${n1}"
