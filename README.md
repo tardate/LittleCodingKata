6d8c1d7dacd786f3fb4c0a827bd3a99fd1f65958
Paul Gallagher <gallagher.paul@gmail.com>
2026-05-28 12:44:16 +0800

    [perl/shuffle-line] add notes (#394)
    * [perl/shuffle-line] add notes
    
    Signed-off-by: Paul Gallagher <gallagher.paul@gmail.com>
    
    * add to catalog
    
    Signed-off-by: Paul Gallagher <gallagher.paul@gmail.com>
    
    * update catalog entry
    
    Signed-off-by: Paul Gallagher <gallagher.paul@gmail.com>
    
    ---------
    
    Signed-off-by: Paul Gallagher <gallagher.paul@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# 458 Little Coding Katas
+# 459 Little Coding Katas
 
-> Latest addition :sparkles: [Test::More](./perl/test-more) - The basics of Perl testing with the Test::More module.
+> Latest addition :sparkles: [shuffleLine](./perl/shuffle-line) - Using Perl to move every nth customer to the end of the queue; cassidoo's interview question of the week (2026-05-25).
 
 Like many programmers, I have a folder that I've been carrying around for years
 into which I've squirrelled away all manner of tests, notes, tips and tricks. Occasionally I've thought about
---
+++ perl/shuffle-line/challenge.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+#
+use strict;
+use warnings;
+
+sub shuffleLine {
+  my ($names, $n) = @_;
+  return @$names if $n <= 0;
+  my @result = ();
+  my @endOfLine = ();
+  for (my $i = 0; $i < @$names; $i++) {
+    if (($i + 1) % $n == 0) {
+      push @endOfLine, $names->[$i];
+    } else {
+      push @result, $names->[$i];
+    }
+  }
+  push @result, @endOfLine;
+  return @result;
+}
+
+if (!caller()) {
+  if (@ARGV != 2) {
+    print "Usage: perl challenge.pl \"<csv-names>\" <integer-n>\n";
+    exit 1;
+  }
+
+  my ($names_string, $n) = @ARGV;
+  my @names = split(/,/, $names_string);
+  @names = map { s/^\s+|\s+$//g; $_ } @names;
+  print "# given names=[" . join(',', @names) . "] and n=$n, result is:\n";
+
+  my @result = shuffleLine(\@names, $n);
+  print "[" . join(',', @result) . "]\n";
+}
+
+1;
